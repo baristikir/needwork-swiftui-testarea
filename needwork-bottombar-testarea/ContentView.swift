05050e3c146e72b98eba5e6fabb61d0c4a619e82
Baris Tikir <brstkr@icloud.com>
2020-08-07 19:36:32 +0200

    Initial Commit

---
+++ needwork-bottombar-testarea/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  needwork-bottombar-testarea
+//
+//  Created by Baris Tikir on 07.08.20.
+//  Copyright © 2020 Baris Tikir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
